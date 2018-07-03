fa216b6e45692336def381b884e28458af0fe209
AMIRmh <amir.haghollahi@gmail.com>
2018-07-03 08:45:20 +0430

    http client struct created.

--- Workspace/src/client/client.go
+++ Workspace/src/client/client.go
@@ -18,32 +18,35 @@ const (
 )
 
 var (
-	specialMessage = 4294967295
-	waitAcksArray = make([]int, 0)
-	windowsSize int
-	udpAddr *net.UDPAddr
-	conns []*net.UDPConn
-	ackArrayMutex sync.Mutex
-	id  = make([]byte, 10)
+	httpClient = HTTPCleint{
+		specialMessage: 4294967295,
+		id: make([]byte, 10),
+	}
 )
+
+type HTTPCleint struct {
+	specialMessage int
+	udpAddr *net.UDPAddr
+	windowsSize int
+	id []byte
+}
 
 type Packet struct {
 	partNumber int
 	data []byte
 }
 
-func InitClient(host ,port string,nth int) {
-	id = []byte(DefaultId)
-	windowsSize = nth
+func InitClient(host ,port string) {
+	httpClient.id = []byte(DefaultId)
 	service := host + ":" + port
-	udpAddr, _ = net.ResolveUDPAddr("udp4", service)
-	getId()
+	httpClient.udpAddr, _ = net.ResolveUDPAddr("udp4", service)
+	httpClient.getId()
 }
 
-func getId() {
-	go fillId()
+func (hc HTTPCleint) getId() {
+	go hc.fillId()
 	for {
-		if string(id) == DefaultId {
+		if string(hc.id) == DefaultId {
 			fmt.Println("retying to connect")
 			time.Sleep(1000 * time.Millisecond)
 		} else {
@@ -52,24 +55,31 @@ func getId() {
 	}
 }
 
-func fillId() {
-	connection := createConnection()
-	asyncSendUDP(id, specialMessage, connection)
+func (hc HTTPCleint) fillId() {
+	connection := hc.createConnection()
+	hc.asyncSendUDP(httpClient.id,httpClient.specialMessage, connection)
 	connection.SetReadDeadline(time.Now().Add(ReadTimeOut))
-	connection.Read(id)
-	if string(id) == DefaultId {
-		fillId()
+	connection.Read(httpClient.id)
+	if string(httpClient.id) == DefaultId {
+		hc.fillId()
 	}
 }
 
-func Send(data []byte) {
-	fmt.Println("sending size")
-	sendSize(len(data))
-	fmt.Println("s=ending chuncks")
-	sendChunk(data)
+func Send(data []byte, typeOfRequest string, nth int) {
+	if typeOfRequest == "DNS" {
+
+	} else if typeOfRequest == "HTTP" {
+		httpClient.windowsSize = nth
+		fmt.Println("sending size")
+		httpClient.sendSize(len(data))
+		fmt.Println("sending chuncks")
+		httpClient.sendChunk(data)
+	} else {
+		fmt.Println("request not supported.")
+	}
 }
 
-func sendChunk(input []byte) {
+func (hc HTTPCleint) sendChunk(input []byte) {
 	// padding
 	var appendArray []byte
 	if len(input) > DataSize {
@@ -84,43 +94,43 @@ func sendChunk(input []byte) {
 	
 	// queue created
 	packetsChannel := make(chan Packet, parts)
-	go partsQueue(input, packetsChannel, parts)
+	go hc.partsQueue(input, packetsChannel, parts)
 	
 	// sending parts in windows size
 	var wg sync.WaitGroup
-	wg.Add(windowsSize)
-	for i := 0; i < windowsSize; i++ {
-		go sendThreadParts(packetsChannel, &wg)
+	wg.Add(httpClient.windowsSize)
+	for i := 0; i < hc.windowsSize; i++ {
+		go hc.sendThreadParts(packetsChannel, &wg)
 	}
 	wg.Wait()
 
 	// end message to close the connection.
 	fmt.Println("sending end to server")
-	syncSendUDP([]byte("end"), specialMessage, createConnection())
+	hc.syncSendUDP([]byte("end"), hc.specialMessage, hc.createConnection())
 	fmt.Println("finished sending")
 }
 
-func partsQueue(input []byte, ch chan Packet, parts int) {
+func (hc HTTPCleint) partsQueue(input []byte, ch chan Packet, parts int) {
 	for i := 0; i < parts; i++ {
 		ch <- Packet{i, input[i * DataSize: (i + 1) * DataSize]}
 	}
 	close(ch)
 }
 
-func sendThreadParts(ch chan Packet, wgThread *sync.WaitGroup) {
+func (hc HTTPCleint) sendThreadParts(ch chan Packet, wgThread *sync.WaitGroup) {
 	defer wgThread.Done()
-	conn := createConnection()
+	conn := hc.createConnection()
 	for packet := range ch {
-		syncSendUDP(packet.data, packet.partNumber, conn)
+		hc.syncSendUDP(packet.data, packet.partNumber, conn)
 	}
 	fmt.Println("send part finished")
 }
 
-func asyncSendUDP(dataUdp []byte, part int, udpConn *net.UDPConn) {
+func (hc HTTPCleint) asyncSendUDP(dataUdp []byte, part int, udpConn *net.UDPConn) {
 	arr := make([]byte, PartSize)
 	binary.BigEndian.PutUint32(arr, uint32(part))
 	arr = append(arr, dataUdp...)
-	data := id
+	data := hc.id
 	data = append(data, arr...)
 	_, err := udpConn.Write(data)
 	if err != nil {
@@ -129,33 +139,33 @@ func asyncSendUDP(dataUdp []byte, part int, udpConn *net.UDPConn) {
 	}
 }
 
-func syncSendUDP(data []byte, part int, udpConn *net.UDPConn) {
-	asyncSendUDP(data, part, udpConn)
+func (hc HTTPCleint) syncSendUDP(data []byte, part int, udpConn *net.UDPConn) {
+	hc.asyncSendUDP(data, part, udpConn)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	retrySendUDP(data, part, udpConn, &wg)
+	hc.retrySendUDP(data, part, udpConn, &wg)
 	wg.Wait()
 }
 
-func retrySendUDP(data []byte, part int, udpConn *net.UDPConn, wg *sync.WaitGroup) {
+func (hc HTTPCleint) retrySendUDP(data []byte, part int, udpConn *net.UDPConn, wg *sync.WaitGroup) {
 	buf := make([]byte, 10)
 	udpConn.SetReadDeadline(time.Now().Add(ReadTimeOut))
 	_, err := udpConn.Read(buf[0:])
 
 	if err != nil {
-		asyncSendUDP(data, part, udpConn)
-		go retrySendUDP(data, part, udpConn, wg)
+		hc.asyncSendUDP(data, part, udpConn)
+		go hc.retrySendUDP(data, part, udpConn, wg)
 	} else {
 		wg.Done()
 	}
 }
 
-func sendSize(size int) {
-	syncSendUDP([]byte(strconv.Itoa(size)), specialMessage, createConnection())
+func (hc HTTPCleint) sendSize(size int) {
+	hc.syncSendUDP([]byte(strconv.Itoa(size)), hc.specialMessage, hc.createConnection())
 }
 
-func createConnection() *net.UDPConn {
-	connection, err := net.DialUDP("udp", nil, udpAddr)
+func (hc HTTPCleint) createConnection() *net.UDPConn {
+	connection, err := net.DialUDP("udp", nil, hc.udpAddr)
 	myLib.CheckError(err)
 	return connection
 }
